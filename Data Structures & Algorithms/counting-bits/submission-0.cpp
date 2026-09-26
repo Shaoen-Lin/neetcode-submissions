2cd7b927cf9f157310dfacbd0fe771a909562047
林劭恩 <60027202+Shaoen-Lin@users.noreply.github.com>
2026-09-26 13:16:14 +0800

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+
+        vector<int> ans;
+        for(int i=0 ; i<=n ; ++i)
+        {
+            ans.push_back(count(i));
+        }
+
+        return ans;
+    }
+
+    int count(int n)
+    {
+        int cnt=0;
+        while(n>0)
+        {
+            cnt += (n&1);
+            n >>= 1;
+        }
+
+        return cnt;
+    }
+};
